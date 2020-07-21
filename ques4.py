624f5ed424d68a2af79e6fe0c9dd34ceb5b9826b
Aayush Dip Giri <invincibleaayu@.github.com>
2020-07-21 18:26:33 +0545

    solution of ques 4

---
+++ ques4.py
@@ -0,0 +1,22 @@
+import numpy as np
+import math
+#we have
+g=9.8#acceleration due to gravity
+ho=10#height
+np_height=np.arange(10,0,-0.5)
+print(np_height)
+#we need to calculate the sequence of time at a 
+# given height which is stored in numpy array
+time=[]
+for y in np_height:
+    t="%.2f"%((math.sqrt(2*(ho-y)))/g)
+    time.append(t)
+
+print("The sequence of time in a givin height is",time)
+#now we calcaulate the average velocity
+del_y=-0.5 #for each interval according to ques
+average_velocity=[]
+for t in range(0,(len(time)-1)):
+    average_velocity.append("%.2f"%(del_y/(float(time[t+1])-float(time[t]))))
+print("The average velocity is ",average_velocity)
+print(len(average_velocity))
