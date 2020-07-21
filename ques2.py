30171daa4c108c5c469bc81ea97e32d7dee2d0ac
Aayush Dip Giri <invincibleaayu@.github.com>
2020-07-21 17:10:43 +0545

    solution of ques 2

---
+++ ques2.py
@@ -0,0 +1,8 @@
+import math
+#we need to import math to calculate such values
+#soln of i
+print("The value of i is","%.2f"%((2+math.exp(2.8))/(math.sqrt(13)-2)))
+#soln of ii
+print("the value of ques ii is","%.2f"%((1-(1+math.log(2))**(-3.5))/(1+math.sqrt(5))))
+#soln of iii
+print("the value of iii is ","%.2f"%(math.sin((2-math.sqrt(2))/(2+math.sqrt(2)))))
