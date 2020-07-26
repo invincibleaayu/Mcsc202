0d14b29108f6c7266478096b94591ac780d61996
Aayush Dip Giri <invincibleaayu@.github.com>
2020-07-26 08:46:55 +0545

    solution of ques no 9

---
+++ ques7.py
@@ -0,0 +1,13 @@
+# Write a program to calculate the sum of natural n natural numbers 1 + 2 + ⋯ + n.
+# Calculate the sum when n = 10
+
+number=int(input("Please enter the number upto which you want find the sum:"))
+#now we calculate the sum
+def calc_sum(number):
+    sum=0
+    for num in range(1,number+1):
+        sum+=num
+    return sum
+
+c=calc_sum(number)
+print("the sum upto the number {} is {} ".format(number,c))
